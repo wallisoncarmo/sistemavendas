ad171b24e78337728f9ebb0bdd4c4ed42b498895
Wallison do Carmo <wallisoncarmo01@gmail.com>
2018-01-13 18:28:40 -0200

    Projeto Criado

---
+++ src/main/java/com/wallison/sistemavendas/SistemavendasApplication.java
@@ -0,0 +1,12 @@
+package com.wallison.sistemavendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemavendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemavendasApplication.class, args);
+	}
+}
