208d81d6779f6aece233bb776684d018739f486e
Wallison do Carmo <wallisoncarmo01@gmail.com>
2018-01-14 01:05:25 -0200

    Produto e acossiacao n para n com categoria

--- src/main/java/com/wallison/sistemavendas/SistemavendasApplication.java
+++ src/main/java/com/wallison/sistemavendas/SistemavendasApplication.java
@@ -8,13 +8,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.wallison.sistemavendas.domain.Categoria;
+import com.wallison.sistemavendas.domain.Produto;
 import com.wallison.sistemavendas.repositoties.CategoriaRepository;
+import com.wallison.sistemavendas.repositoties.ProdutoRepository;
 
 @SpringBootApplication
 public class SistemavendasApplication implements CommandLineRunner {
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	@Autowired
+	private ProdutoRepository produtoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SistemavendasApplication.class, args);
@@ -26,7 +30,19 @@ public class SistemavendasApplication implements CommandLineRunner {
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
 
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+
 		categoriaRepository.save(Arrays.asList(cat1, cat2));
+		produtoRepository.save(Arrays.asList(p1, p2, p3));
 
 	}
 }
---
+++ src/main/java/com/wallison/sistemavendas/repositoties/ProdutoRepository.java
@@ -0,0 +1,13 @@
+package com.wallison.sistemavendas.repositoties;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.wallison.sistemavendas.domain.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+	
+	
+
+}
