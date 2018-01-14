73d36aa882ee354cc7d1ce144874652f998aea7f
Wallison do Carmo <wallisoncarmo01@gmail.com>
2018-01-14 17:05:26 -0200

    [ADD] Endpoint /pedido/{id}

---
+++ src/main/java/com/wallison/sistemavendas/services/PedidoService.java
@@ -0,0 +1,27 @@
+package com.wallison.sistemavendas.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.wallison.sistemavendas.domain.Pedido;
+import com.wallison.sistemavendas.repositoties.PedidoRepository;
+import com.wallison.sistemavendas.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+
+	public Pedido buscar(Integer id) {
+		Pedido obj = repo.findOne(id);
+
+		if (obj == null) {
+			throw new ObjectNotFoundException(
+					"Objeto não encontrado ID: " + id + " Tipo: " + Pedido.class.getName());
+		}
+
+		return obj;
+	}
+
+}
