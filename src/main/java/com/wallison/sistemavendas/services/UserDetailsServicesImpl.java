3699e61bcdb85b73e3dce1450c8e5e248e84d9bd
Wallison do Carmo <wallisoncarmo01@gmail.com>
2018-01-21 12:34:47 -0200

    [ADD] Implementacao da autenticacao e geracao do token JWT

---
+++ src/main/java/com/wallison/sistemavendas/services/UserDetailsServicesImpl.java
@@ -0,0 +1,27 @@
+package com.wallison.sistemavendas.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.wallison.sistemavendas.domain.Cliente;
+import com.wallison.sistemavendas.repositoties.ClienteRepository;
+import com.wallison.sistemavendas.security.UserSS;
+
+@Service
+public class UserDetailsServicesImpl implements UserDetailsService {
+	@Autowired
+	private ClienteRepository repo;
+
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		Cliente cli = repo.findByEmail(email);
+		if (cli == null) {
+			throw new UsernameNotFoundException(email);
+		}
+		return new UserSS(cli.getId(), cli.getEmail(), cli.getSenha(), cli.getPerfis());
+	}
+
+}
